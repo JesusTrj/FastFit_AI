8a52437844ab80d6254d3072ba7ab0a58cb393db
Jesús A. Trujillo <69176699+JesusTrj@users.noreply.github.com>
2022-09-24 14:38:19 -0500

    Design

---
+++ Design/Components/outfitGeneratorCategories/outfitGeneratorCategories.jsx
@@ -0,0 +1,100 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./outfitGeneratorCategories.css";
+
+class OutfitGeneratorCategories extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="66b0f66c-71eb-46d7-9242-f8581aad4d4a" className="outfitGeneratorCategories">        <div data-layer="51cc58a0-7aea-414e-86f3-7b17c404dd5c" className="cochitiLake"></div>
+        <div data-layer="cadb4e3e-f3b0-4756-95ad-31b51b0acd81" className="how">How?</div>
+        <div data-layer="383ecb9a-a113-4c12-9d22-f2dad2b90a22" className="toogleChips">            <div data-layer="6c3016e9-5354-43f9-a925-365d7d6919de" className="chipnight">                <div data-layer="ff74c430-4466-4f5e-b3b4-bad61a894e2a" className="night2a4c5175">                    <div data-layer="1bfc0b05-beb1-4375-9e57-202b5364b506" className="container34072b1e">                        <div data-layer="91e847a1-3563-44c0-90a4-c22057d3f2d5" className="xElevation7351d6a1"></div>
+                        <div data-layer="374ea85f-8760-44f5-a243-b4f4c0cb4a10" className="xColorLContainere8679b28"></div>
+                        <div data-layer="ce21d9a6-4118-4333-bb7c-f3cd32dbd558" className="statesf8bae0d1"></div>
+</div>
+                    <div data-layer="23476554-aaa7-4e85-9441-38589f66cee0" className="xLabel0ea58551">Night</div>
+</div>
+                <svg data-layer="44c22e39-6b2a-4d38-8fa4-699825ca6b3d" preserveAspectRatio="none" viewBox="5.114999771118164 8.385000228881836 26.385009765625 20.114990234375" className="iconMaterialCheckb2429543"><path d="M 13.5 24.2549991607666 L 7.244999885559082 18 L 5.114999771118164 20.11499977111816 L 13.5 28.5 L 31.5 10.5 L 29.38500022888184 8.385000228881836 L 13.5 24.2549991607666 Z"  /></svg>
+</div>
+            <div data-layer="6732e7ba-bd65-4206-9885-53657e5f530e" className="day">                <div data-layer="ea0b342a-735d-4dff-bc31-a854d31927fd" className="containerd1823ad4">                    <div data-layer="0679b158-06b6-4657-840f-46e861f1849a" className="xElevation752548d6"></div>
+                    <div data-layer="a9c6ec5b-139d-496f-a74d-19a6a3659a69" className="xColorLContainer2a263a71"></div>
+                    <div data-layer="9f5f9016-ea41-45b4-b7ce-4065e25a9b9c" className="states348e18ac"></div>
+</div>
+                <div data-layer="2794767e-3b85-4da8-b008-27209f8f2396" className="xLabel4d79bfc4">Day</div>
+</div>
+            <div data-layer="e99f7e10-c204-4d04-8527-3e2572b64374" className="warm">                <div data-layer="6d530fe9-2345-431f-9648-dae120c63ab3" className="container3e1555ec">                    <div data-layer="10d59271-96dc-4158-9aa2-666ac7897c43" className="xElevationdf1b3860"></div>
+                    <div data-layer="8c96ae9a-ecdc-4dda-8a1c-d186e0298cb2" className="xColorLContainera4ad8e91"></div>
+                    <div data-layer="378e102f-c37b-4062-b777-c287d2b1cfcf" className="statesaa8b29d3"></div>
+</div>
+                <div data-layer="74ce9937-fc2f-4eba-8a0b-2724eb620f6c" className="xLabel731a77fd">Warm</div>
+</div>
+            <div data-layer="c4d57c23-90ca-4ca2-8345-5f475a46d6a7" className="chipcool">                <div data-layer="d5272c0e-1737-42ef-a81b-c2ee164fe52b" className="night8e9d8080">                    <div data-layer="bc5f2a8b-60a9-45c1-b577-b32a2f7ca633" className="container8d552bde">                        <div data-layer="399140d0-4058-4714-8410-59d7e4ac7ba1" className="xElevation6d5e9fdc"></div>
+                        <div data-layer="c77c6953-9b95-4169-bbca-d35160bf83e3" className="xColorLContainer2692b547"></div>
+                        <div data-layer="f9b3f1a2-7f6d-4ae7-aa61-2f4e335f5627" className="states7f2ed2cb"></div>
+</div>
+                    <div data-layer="493fc194-1a44-4716-a560-0b9799b4e6ae" className="xLabel561b6862">Cool</div>
+</div>
+                <svg data-layer="0df19cae-d5cd-4daa-84f7-140f0aa14a2b" preserveAspectRatio="none" viewBox="5.114999771118164 8.385000228881836 26.385009765625 20.114990234375" className="iconMaterialChecka73a191b"><path d="M 13.5 24.2549991607666 L 7.244999885559082 18 L 5.114999771118164 20.11499977111816 L 13.5 28.5 L 31.5 10.5 L 29.38500022888184 8.385000228881836 L 13.5 24.2549991607666 Z"  /></svg>
+</div>
+            <div data-layer="e39efad4-ddca-4637-8d71-db2a5a70ead1" className="casual">                <div data-layer="12ac82ce-d41f-42e6-87aa-371e07bc818d" className="containerecbbf8e9">                    <div data-layer="37e7e610-39b1-450c-9ed7-3d5e786c315b" className="xElevation8b9950f9"></div>
+                    <div data-layer="e0115f29-e672-45b7-9c5b-369a55afa1e5" className="xColorLContainer6e20a68e"></div>
+                    <div data-layer="21254cbd-8caa-4709-8c4c-2121929e54cb" className="statesf87f07f7"></div>
+</div>
+                <div data-layer="353af80f-2d25-4ea8-9fc0-88fe98f386d8" className="xLabel6d5556d4">Casual</div>
+</div>
+            <div data-layer="1fa37871-01ab-401f-b55e-40fbf40ae70c" className="chipformal">                <div data-layer="d647bae2-1e47-4fab-b3fe-4ad29150466b" className="night26f79a8e">                    <div data-layer="03b0aefa-c348-49b2-99f1-bc5892fb4ea5" className="container74462534">                        <div data-layer="2255b8ea-79c1-4c83-b19d-5045893ca3c0" className="xElevation9b15b7f6"></div>
+                        <div data-layer="82799be1-c837-4d0f-9d78-d2218b219fa4" className="xColorLContainer565f4144"></div>
+                        <div data-layer="049f88f2-49be-43c0-9a15-b67bd62c1483" className="states359bf247"></div>
+</div>
+                    <div data-layer="aa224e3e-1f36-46dd-8ba3-2953e24bda8b" className="xLabel8f98ffb1">Formal</div>
+</div>
+                <svg data-layer="c7d9684a-d07a-4400-af31-ebdca3be90a3" preserveAspectRatio="none" viewBox="5.114999771118164 8.385000228881836 26.385009765625 20.114990234375" className="iconMaterialCheck455cfdaa"><path d="M 13.5 24.2549991607666 L 7.244999885559082 18 L 5.114999771118164 20.11499977111816 L 13.5 28.5 L 31.5 10.5 L 29.38500022888184 8.385000228881836 L 13.5 24.2549991607666 Z"  /></svg>
+</div>
+            <div data-layer="ffc0c3ef-c65e-4973-bf17-d426f379aa87" className="sport">                <div data-layer="4f924108-a8e5-4f09-836b-7e90ab5f214c" className="containerf3b8ec12">                    <div data-layer="6ca04c8e-52cf-4b42-80c2-16ee47fd2896" className="xElevationadee45b6"></div>
+                    <div data-layer="b6762328-a47b-49ea-b19d-5e5ab65fa301" className="xColorLContaineree53c5c9"></div>
+                    <div data-layer="6069ffd6-c57d-4bc9-8379-3a4c175241b6" className="states1216f041"></div>
+</div>
+                <div data-layer="e8bba44c-d57b-4c47-a642-79bab8ede952" className="xLabel9401097f">Sport</div>
+</div>
+            <div data-layer="784d9c29-290f-41fe-a918-2bd7f1cadb81" className="rainy">                <div data-layer="3bdef234-3f4f-4313-904b-7cb711efadb0" className="container92c24870">                    <div data-layer="c69a2acf-be4f-4453-a474-f4bad5daedb8" className="xElevationdf4020a3"></div>
+                    <div data-layer="0775a8c7-8877-4ad5-ac54-f07fbc6c5d2d" className="xColorLContainer9c7b8317"></div>
+                    <div data-layer="28d9b4e3-3795-41a7-be71-d0bbd345888a" className="statesdd7d4e08"></div>
+</div>
+                <div data-layer="1b2ef0c8-3a3a-4d8b-afbd-790b3034e41d" className="xLabel6dbcb186">Rainy</div>
+</div>
+            <div data-layer="33dbbf60-74f5-4335-ab50-851ea6405a4a" className="chipdry">                <div data-layer="188c589c-d01f-4271-b808-d1b4a8572ffe" className="night">                    <div data-layer="9c8c6295-079c-45c7-b2fe-e73e406a7150" className="container">                        <div data-layer="348977ca-5a46-4f4e-aff9-7ce5c6c6ce54" className="xElevation"></div>
+                        <div data-layer="6efdb230-4260-4186-ba78-99d9538e7620" className="xColorLContainer"></div>
+                        <div data-layer="0b1a7282-0396-4760-b8a8-1914b8f7a277" className="states"></div>
+</div>
+                    <div data-layer="4bb0b1d2-ee6f-4972-9c15-2e795a1dc13a" className="xLabel">Dry</div>
+</div>
+                <svg data-layer="4ceaebd4-f993-4a86-a550-e09046652635" preserveAspectRatio="none" viewBox="5.114999771118164 8.385000228881836 26.385009765625 20.114990234375" className="iconMaterialCheck"><path d="M 13.5 24.2549991607666 L 7.244999885559082 18 L 5.114999771118164 20.11499977111816 L 13.5 28.5 L 31.5 10.5 L 29.38500022888184 8.385000228881836 L 13.5 24.2549991607666 Z"  /></svg>
+</div>
+</div>
+</div>
+
+    );
+  }
+}
+
+OutfitGeneratorCategories.propTypes = {
+
+}
+
+OutfitGeneratorCategories.defaultProps = {
+
+}
+
+
+export default OutfitGeneratorCategories;
+          
---
+++ Design/Components/outfitGeneratorLoading/outfitGeneratorLoading.jsx
@@ -0,0 +1,40 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./outfitGeneratorLoading.css";
+
+class OutfitGeneratorLoading extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="120cc3ab-5d7b-432f-a62b-2ee610765975" className="outfitGeneratorLoading">        <div data-layer="0cc64517-922d-4d5e-b632-c83a84884863" className="newYork"></div>
+        <div data-layer="7a1fcdbf-84a9-45a4-bd3b-477e34abf42d" className="group29">            <div data-layer="e8166de1-f193-4a9b-a835-1cce914e44c4" className="a6622dfaee5d281cf341d264fa2b3aa2"></div>
+            <div data-layer="7a39ff00-61e7-4db6-be06-b11fd0216719" className="inAFewSecondsYoullLookAmazing">In a few seconds you'll look amazing</div>
+            <div data-layer="5a2dcee5-673c-4dc8-953a-2ec644873983" className="loading">Loading...</div>
+</div>
+</div>
+
+    );
+  }
+}
+
+OutfitGeneratorLoading.propTypes = {
+
+}
+
+OutfitGeneratorLoading.defaultProps = {
+
+}
+
+
+export default OutfitGeneratorLoading;
+          
