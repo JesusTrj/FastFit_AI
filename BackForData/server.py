a08b84c38bec5c9b0cb64bd90bb07a75880eee20
Armando <A01570376@itesm.mx>
2022-09-24 16:26:04 -0500

    ✨ Backend for data stuff

---
+++ BackForData/server.py
@@ -0,0 +1,21 @@
+from multiprocessing import cpu_count, freeze_support
+
+import uvicorn
+
+def start_server(host="172.17.196.221",
+                 port=80,
+                 num_workers=4,
+                 loop="asyncio",
+                 reload=True):
+
+    uvicorn.run("web_server:app",
+                host=host,
+                port=port,
+                workers=num_workers,
+                loop=loop,
+                reload=reload)
+
+if __name__ == "__main__":
+    freeze_support()  # Needed for pyinstaller for multiprocessing on WindowsOS
+    num_workers = int(cpu_count() * 0.75)
+    start_server(num_workers=num_workers)
